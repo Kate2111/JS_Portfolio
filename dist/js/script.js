4f6f0a1b7978e4efb1013b056c8299807cd50a5b
Kate <noskova2111@mail.ru>
2022-12-17 20:25:17 +0300

    style

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,18 +1,1 @@
-const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu');
-    close = document.querySelector('.menu__close');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
-
-close.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
-
-const counters = document.querySelectorAll('.skills__ratings-counter'),
-    lines = document.querySelectorAll('.skills__ratings-line span');
-
-counters.forEach((item, i) => {
-    lines[i].style.width = item.innerHTML;
-});
+"use strict";
